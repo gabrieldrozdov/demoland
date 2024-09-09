1d0c4ea7f86d09a195c61b079d884d9493b4337d
Gabriel Drozdov <gabrieldrozdov@gmail.com>
2024-09-09 17:40:51 -0400

    finished demo drafts for first two units of guide

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function scrollPage(delta) {
 	const container = document.querySelector('.container');
 	container.scrollLeft += delta;
 }
+
+// Hover effect
+let sections = document.querySelectorAll('.section');
+for (let section of sections) {
+	section.addEventListener('mouseenter', () => {
+		for (let section of sections) {
+			section.dataset.active = 0;
+		}
+		section.dataset.active = 1;
+	})
+	section.addEventListener('mouseleave', () => {
+		for (let section of sections) {
+			section.dataset.active = 1;
+		}
+	})
+}
